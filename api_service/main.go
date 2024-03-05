9d041e3e8e21d30b4d7d5fb9ffbf069b0e797883
wuwanhao <1461282363@qq.com>
2024-03-05 23:53:43 +0800

    add versions api

--- api_service/main.go
+++ api_service/main.go
@@ -4,6 +4,7 @@ import (
 	"api_service/heartbeat"
 	"api_service/locate"
 	"api_service/objects"
+	"api_service/versions"
 	"log"
 	"net/http"
 	"os"
@@ -14,5 +15,6 @@ func main() {
 	go heartbeat.ListenHeartbeat()
 	http.HandleFunc("/objects/", objects.Handler)
 	http.HandleFunc("/locate/", locate.Handler)
+	http.HandleFunc("/versions/", versions.Handler)
 	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
 }
---
+++ api_service/versions/versions.go
@@ -0,0 +1,46 @@
+package versions
+
+import (
+	"connector/es"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	m := r.Method
+	if m != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	from := 0
+	size := 1000
+	name := strings.Split(r.URL.EscapedPath(), "/")[2] // 拿到key
+	// 阻塞
+	for {
+		metas, err := es.GetAllVersions(name, from, size)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		for i := range metas {
+			// 得到字节流
+			b, _ := json.Marshal(metas[i])
+			// 写入响应正文
+			w.Write(b)
+			w.Write([]byte("\n"))
+		}
+
+		// 当前批是最后一批数据，metas的大小是小于size的，终止循环，返回最终的数据
+		if len(metas) != size {
+			return
+		}
+
+		from += size
+	}
+
+}
